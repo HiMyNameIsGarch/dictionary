7e17ac68900489ff5c16be120796dd52d959dbf9
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-02-09 19:59:21 +0200

    implement functions to draw axis of the graph

--- Models/Graphs/Bar.cs
+++ Models/Graphs/Bar.cs
@@ -3,34 +3,44 @@ public class Bar
     public int Min;
     public int Max;
     public int Rate;
+    public int[] Values;
     private bool CanBuildBar = false;
 
     public Bar(int min, int max, int rate)
     {
         if(min >= max)
         {
-            Console.WriteLine("Min value is bigger than the max one!");
+            Console.WriteLine("Min value is bigger than max one");
         }
         else if(((max - min) / rate) < 3)
         {
-            Console.WriteLine("The values are smaller than 3");
+            Console.WriteLine("To little y coordonates");
         }
         else
         {
             CanBuildBar = true;
         }
+        Max = max;
+        Min = min;
+        Rate = rate;
+        Values = new int[0];
     }
-    public int[] GetValues()
+    public void ComputeValues()
     {
-        if(!CanBuildBar) return new int[0];
-        var values = new int[((Max - Min) / Rate) + 1];
+        if(!CanBuildBar)
+        {
+            Values = new int[0];
+            return;
+        } 
+
+        var num = ((Max - Min) / Rate) + 1;
+        var values = new int[num];
         int j = 0;
-        for(int i = Min; i < Max; i += Rate)
+        for(int i = Min; i <= Max; i += Rate)
         {
             values[j] = i;
             j++;
         }
-        return values;
+        Values = values;
     }
 }
-
--- Models/Graphs/Graph.cs
+++ Models/Graphs/Graph.cs
@@ -1,41 +1,97 @@
 public class Graph
 {
-    private int[] _left;
-    private int[] _bottom;
-    private int[] _values;
+    private Bar _yaxis;
+    private Bar _xaxis;
+    private int[] _coordonates;
     private int[] _axis;
 
-    public Graph(Bar left, Bar bottom, int[] values)
+    public Graph(Bar yaxis, Bar xaxis, int[] coordonates)
     {
-        _left = left.GetValues();
-        _bottom = bottom.GetValues();
-        _values = values;
-        _axis = new int[_values.Length];
+        _yaxis = yaxis;
+        _yaxis.ComputeValues();
+        _xaxis = xaxis;
+        _xaxis.ComputeValues();
+        _coordonates = coordonates;
+        _axis = new int[_coordonates.Length];
     }
 
     private bool CanDrawGraph()
     {
-        if(Console.WindowWidth < _values.Length)
+
+        if(Console.WindowWidth < _xaxis.Values.Length)
         {
-            Console.WriteLine("Values bigger than screen, abording...");
+            Console.WriteLine("Values are bigger than screen, abording...");
+            return false;
+        }
+        if(_yaxis.Values.Length == 0 || _xaxis.Values.Length == 0)
+        {
+            Console.WriteLine("Cannot get the values for the outsides");
+            return false;
+        }
+        if(_coordonates.Length != _xaxis.Values.Length)
+        {
+            Console.WriteLine("The values provided aren't equal to the _xaxis table");
             return false;
         }
         return true;
     }
 
-    private void DisplayLeftPart(int[] bounds)
+    private string BuildSpaces(int max, int current)
     {
+        string s = "";
+        var maxSpaces = (max == current) ? 1 : max - current;
+        for(int i = 0; i < maxSpaces; i++)
+        {
+            s += " ";
+        }
+        return s;
     }
-    private void DisplayBottomPart(int[] values)
+
+    private string BuildSpaces(int max)
     {
+        string s = "─";
+        for(int i = 0; i < max; i++)
+        {
+            s += s;
+        }
+        return s;
+    }
+
+    private void DisplayLeftPart()
+    {
+        var numLength = _yaxis.Max.ToString().Length + 1;
+        for(int i = _yaxis.Max; i >= _yaxis.Min; i -= _yaxis.Rate)
+        {
+            Console.WriteLine(i + BuildSpaces(numLength, i.ToString().Length) + "│");
+        }
+        Console.Write(BuildSpaces(numLength, _yaxis.Min.ToString().Length) + " " + "└");
+    }
+    private void DisplayBottomPart()
+    {
+        Console.Write("─");
+        var current = Console.GetCursorPosition();
+        int j = 0;
+        for(int i = _xaxis.Min; i <= _xaxis.Max; i += _xaxis.Rate)
+        {
+            var aj = Console.GetCursorPosition();
+            Console.Write(BuildSpaces(i.ToString().Length));
+            Console.SetCursorPosition(aj.Left, aj.Top + 1);
+            j++;
+            _axis[j - 1] = aj.Left;
+            Console.Write(i);
+            Console.SetCursorPosition(aj.Left + i.ToString().Length + 1, aj.Top);
+        }
+        Console.Write("\n");
+        Console.SetCursorPosition(current.Left, current.Top + 2);
+        Console.Write("\n");
     }
 
     private void DisplayTable()
     {
         // Display left part
-        DisplayLeftPart(_left);
+        DisplayLeftPart();
         // Display bottom part
-        DisplayBottomPart(_values);
+        DisplayBottomPart();
     }
 
     private void DisplayValues(int left, int top)
@@ -47,7 +103,7 @@ public class Graph
         if(!CanDrawGraph()) return;
 
         var beforeTable = Console.GetCursorPosition();
-        beforeTable.Left += _left[_left.Length].ToString().Length;
+        beforeTable.Left += _yaxis.Max.ToString().Length;
 
         DisplayTable();
 
