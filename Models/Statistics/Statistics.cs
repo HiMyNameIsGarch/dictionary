43757a7472f1b0119d140c0560aecdf9640fad6a
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-02-14 20:10:30 +0200

    display average statistics for all session files

--- Models/Statistics/Statistics.cs
+++ Models/Statistics/Statistics.cs
@@ -29,6 +29,7 @@ public class Statistics
     {
         WriteLine("Display the evolution for: ");
         WriteLine("1. Statistics for very last session");
+        WriteLine("2. Average Statistics of all sessions");
         Write("Press the number of the option: ");
         char key = Console.ReadKey().KeyChar;
         Write("\n\n");
@@ -43,6 +44,9 @@ public class Statistics
             case '1':
                 StatisticsForLastSession(data.Last());
                 break;
+            case '2':
+                AvarageStatisticsForAllSessions(data);
+                break;
            default: 
                 break;
         }
@@ -52,21 +56,46 @@ public class Statistics
     {
         WriteLine("Filename: " + lastSession.FileName);
         WriteLine("Mode: " + lastSession.Mode.ToFormattedString());
-        var timeResponses = lastSession.TimeResponses.Select(t => (int)t).ToArray();
 
+        var timeResponses = lastSession.TimeResponses.Select(t => (int)t).ToArray();
         WriteLine("\nEvolution of response time: ");
-        var rleft = new Bar(0, 5, 1);
-        var rright = new Bar(1, timeResponses.Length, 1);
-        var graph = new Graph(rleft, rright, timeResponses, true);
+        Graph graph = GetResponseTimeGraph(timeResponses);
         graph.Draw();
 
         var accuracies = lastSession.Accuracies.Select(a => (int)a).ToArray();
         WriteLine("Evolution of accuracy: ");
-        var aleft = new Bar(10, 100, 10);
-        var aright = new Bar(1, accuracies.Length, 1);
-        var agraph = new Graph(aleft, aright, accuracies, false);
+        Graph agraph = GetAccuracyGraph(accuracies);
         agraph.Draw();
 
+    }
+
+    private void AvarageStatisticsForAllSessions(ICollection<StatisticModel> data)
+    {
+        int[] times = data.Select(s => (int)s.AverageResponseTime).ToArray();
+        WriteLine("\nEvolution of response time: ");
+        Graph graph = GetResponseTimeGraph(times);
+        graph.Draw();
+
+        int[] acc = data.Select(s => (int)s.AverageAccuracy).ToArray();
+        WriteLine("Evolution of accuracy: ");
+        Graph agraph = GetAccuracyGraph(acc);
+        agraph.Draw();
+    }
+
+    private Graph GetResponseTimeGraph(int[] timeResponses)
+    {
+        var yaxis = new Bar(0, 5, 1);
+        var xaxis = new Bar(1, timeResponses.Length, 1);
+        var graph = new Graph(yaxis, xaxis, timeResponses, true);
+        return graph;
+    }
+
+    private Graph GetAccuracyGraph(int[] accuracies)
+    {
+        var yaxis = new Bar(10, 100, 10);
+        var xaxis = new Bar(1, accuracies.Length, 1);
+        var graph = new Graph(yaxis, xaxis, accuracies, false);
+        return graph;
     }
 
     private ICollection<StatisticModel> GetAllStatistics()
