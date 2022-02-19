02744664d57ec7198f6c7baeba34f57aed9e3865
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-02-19 12:57:44 +0200

    clear the message on persistent mode

--- Models/Modes/Persistent.cs
+++ Models/Modes/Persistent.cs
@@ -1,5 +1,9 @@
+using static ConsoleHelper;
+
 public class Persistent: IMode 
 {
+    private const string _errorMessage = "Oops, not finished yet, you still got some words that you don't know yet";
+
     public void Start(ISession session)
     {
         session.BeforeSessionHook();
@@ -10,11 +14,19 @@ public class Persistent: IMode
             pairs = session.Data.WrongPairs;
             if(pairs.Count == 0) session.Stats.Store(session.Data);
             session.AfterSessionHook();
-            if(pairs.Count != 0)
-            {
-                Console.WriteLine("\nOops, not finished yet, you still got some words that you don't know yet\n");
-            }
+            ContinueWithWrongPairs(pairs.Count);
         }
         while(pairs.Count > 0);
+    }
+
+    private void ContinueWithWrongPairs(int pairsCount)
+    {
+        if(pairsCount == 0) return;
+
+        int cursorTop = Console.CursorTop;
+        ColorWriteLine(_errorMessage, ConsoleColor.Red);
+        PressKeyToContinue("Press any key to continue: ");
+        ClearScreen(cursorTop);
+        Console.Write("\n");
     }
 }
