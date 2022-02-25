f7c5223f71953f8f315583682dd7ae5aa71fb4c8
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-02-25 09:21:50 +0200

    catch all errors within the application

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿public class Program
+﻿using static ConsoleHelper;
+
+public class Program
 {
+    private const string _errorMessage = "> Ooops, what a shame, I broke, and now you can't learn properly, here is the error message: ";
+
     public static int Main(string[] args)
     {
-        return Sessionizer.Initiate(args);
+        try
+        {
+            return Sessionizer.Initiate(args);
+        }
+        catch(Exception ex)
+        {
+            ColorWriteLine(_errorMessage, ConsoleColor.Red);
+            ColorWriteLine("> " + ex.Message, ConsoleColor.Magenta);
+            return 1;
+        }
     }
 }
